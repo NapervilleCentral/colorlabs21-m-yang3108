6995f9a91868d992ea69ebeef769d92c4fe661e1
Michael Yang <89219107+m-yang3108@users.noreply.github.com>
2021-11-11 14:14:16 -0600

    colorlab almostdone

--- ColorLabs17/ColorLab.java
+++ ColorLabs17/ColorLab.java
@@ -29,26 +29,118 @@ public class ColorLab
             spot.setGreen(255);
         
     }
+    swan.explore();
     
+        swan = new Picture("images/swan.jpg");
+        pixels = swan.getPixels(); 
+        
+        
+        for ( Pixel spot : pixels)
+        {
+            int number = 0;
+            spot.setRed(255-spot.getRed());
+            spot.setBlue(255-spot.getBlue());
+            spot.setGreen(255-spot.getGreen());
+            
+            
+        }
+        
+        swan.explore();
+        
+         swan = new Picture("images/swan.jpg");
+         pixels = swan.getPixels(); 
+        
+        for ( Pixel spot : pixels)
+        {
+            int r = spot.getRed();
+            int g = spot.getGreen();
+            int b = spot.getBlue();
+            int doubler = r*2;
+            int doubleg = g*2;
+            int doubleb = b*2;
+            
+            if (doubler > 255) {
+                spot.setRed(255);
+            }
+            
+            else if (doubleb > 255) {
+                spot.setBlue(255);
+            }
+            else if (doubleg > 255) {
+                spot.setGreen(255);
+            }
+            else {
+            spot.setColor(new Color(doubler, doubleg, doubleb));
+        }
+            
+        }
+        
+        swan.explore();
+        
+        
         swan = new Picture("images/swan.jpg");
         pixels = swan.getPixels(); 
         
         for ( Pixel spot : pixels)
         {
-            int number = 0;
-            spot.setRed(255-number);
-            spot.setBlue(255-number);
-            spot.setGreen(255-number);
+            int r = spot.getRed();
+            int g = spot.getGreen();
+            int b = spot.getBlue();
             
-            number++;
+            int average = (r+g+b)/3;
+            
+            spot.setColor(new Color(average, average, average));
+            
+            
+
+
         }
         
         swan.explore();
-    
-    
+        
+        swan = new Picture("images/swan.jpg");
+        pixels = swan.getPixels();
+        
+        for (Pixel spot : pixels)
+        {
+            int r = spot.getRed();
+            int g = spot.getGreen();
+            int b = spot.getBlue();
+            
+            int newg =  (int) (g*0.1);
+            int newr = (int) (r*0.1);
+            int newb = (int) (b*0.1);
+            
+            spot.setRed(newr);
+            spot.setGreen(newg);
+            spot.setBlue(newb);
     
     
     
 }
-
+   swan.explore();
+   
+    swan = new Picture("images/swan.jpg");
+        pixels = swan.getPixels();
+        
+        for (Pixel spot : pixels)
+        {
+            int r = spot.getRed();
+            int g = spot.getGreen();
+            int b = spot.getBlue();
+            
+            int newg =  (int) (g*3);
+            int newr = (int) (r*3);
+            int newb = (int) (b*3);
+            
+            spot.setRed(newr);
+            spot.setGreen(newg);
+            spot.setBlue(newb);
+    
+    
+    
+}
+   swan.explore();
+   
+}
 }
