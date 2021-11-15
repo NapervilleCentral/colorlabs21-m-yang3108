f36675100e81bc3143a401dfa58fb810162d937c
Michael Yang <89219107+m-yang3108@users.noreply.github.com>
2021-11-15 14:16:42 -0600

    shepardfairy

--- ColorLabs17/SheparFaireyLab.java
+++ ColorLabs17/SheparFaireyLab.java
@@ -21,99 +21,92 @@ public class SheparFaireyLab
         
          //opens selfie picture 
           /**/
-         Picture face = new Picture("images/shepard.jpg");
-         face.explore();
+         Picture face1 = new Picture("images/shepard1.jpg");         
+         Picture face2 = new Picture("images/shepard.jpg");
+         Picture face3 = new Picture("images/shepard1.jpg");        
          
-         Pixel pixels [] = face.getPixels();
+         Pixel pixels1 [] = face1.getPixels();
+         Pixel pixels2 [] = face2.getPixels();
+         Pixel pixels3 [] = face3.getPixels();
          
-         //change with selfie picture
-         Picture me = new Picture("images/shepard.jpg");
-         Picture me1 = new Picture("images/shepard.jpg");
-         Picture me2 = new Picture("images/shepard.jpg");
+         Color lightgreen = new Color(199,223,137);
+         Color lightyellow = new Color(242,190,87);
+         Color lightblue = new Color(104,121,159);
+         Color pink = new Color(189,116,159);
          
+         int r,g,b,average;
          
-         //method 1 change
-          
-         for (Pixel spot : pixels) {
-             int r = spot.getRed();
-             int g = spot.getGreen();
-             int b = spot.getBlue();
-             
-             Color lightgreen = new Color(199,223,137);
-             Color lightyellow = new Color(242,190,87);
-             Color lightblue = new Color(104,121,159);
-             Color pink = new Color(189,116,159);
-             
-             int average = (int) (r+b+g)/3;
+         //method 3
+         
+         for (Pixel spot : pixels3) {
+             r = spot.getRed();
+             g = spot.getGreen();
+             b = spot.getBlue();
+             average = (int) (r+b+g)/3;
              
              if (average <= 63) 
-                spot.setColor(lightgreen); 
-            
-            
+                spot.setColor(pink); 
              else if (average <= 127) 
-                spot.setColor(lightyellow);
-            
-            
-             else if (average <= 191) 
                 spot.setColor(lightblue);
-            
-            
+             else if (average <= 191) 
+                spot.setColor(lightyellow);
              else if (average <= 255)
-                 spot.setColor(pink);
-                
+                 spot.setColor(lightgreen);
+         }    
              
-                }    
-             
-         face.explore();
+         face3.explore();
    
              
+         //method 2
+         int min = 255;
+         int max = 0;
+         for (Pixel spot : pixels2) {
+             r = spot.getRed();
+             g = spot.getGreen();
+             b = spot.getBlue();
+             average = (int) (r+b+g)/3;
              
-         /**
-          * method 2 change
-          * 
-          */
+             spot.setColor(new Color(average, average, average)); 
+             if (min > spot.getRed()) {
+                 min = spot.getRed();
+             if (max < spot.getRed()) {
+                 max = spot.getRed();
+         }    
          
+         face2.explore();
          
-         //custom color palette
+         //method1
          
-         /**          
-         for (Pixel spot : pixels) {
-             int r = spot.getRed();
-             int g = spot.getGreen();
-             int b = spot.getBlue();
-             
-             Color darkBlue = new Color(10,42,43);
-             Color medBlue = new Color(101,147,160);
-             Color lightBlue = new Color(185,204,184);
-             Color yellow = new Color(255,239,167);
-             Color red = new Color(219,21,34);
-             
-             int average = (int) (r+b+g)/3;
+         Color darkBlue = new Color(10,42,43);
+         Color medBlue = new Color(101,147,160);
+         Color lightBlue = new Color(185,204,184);
+         Color yellow = new Color(255,239,167);
+         Color red = new Color(219,21,34);
+                   
+         for (Pixel spot : pixels1) {
+             r = spot.getRed();
+             g = spot.getGreen();
+             b = spot.getBlue();
+
+             average = (int) (r+b+g)/3;
              
              if (average <= 51) 
                 spot.setColor(darkBlue); 
-            
-            
              else if (average <= 102) 
                 spot.setColor(medBlue);
-            
-            
              else if (average <= 153) 
                 spot.setColor(lightBlue);
-            
-            
              else if (average <= 204)
                  spot.setColor(yellow);
-                
              else if (average <= 255) 
                  spot.setColor(red);
-                
              
-                }    
+         }    
              
-         face.explore();
+         face1.explore();
           
-*/
+         face1.write("images/method1.jpg");
+         face3.write("images/method3.jpg");
          
     }//main       
 }//class
