a0aedf36ffddceee54e71d25632dda2b40541748
Michael Yang <89219107+m-yang3108@users.noreply.github.com>
2021-11-15 21:17:16 -0600

    shepardfinished

--- ColorLabs17/SheparFaireyLab.java
+++ ColorLabs17/SheparFaireyLab.java
@@ -1,8 +1,7 @@
 import java.awt.*;
 import java.util.*;
 /**
- * Write a description of class SheparFaireyLab here.
- * 
+ * Takes an image and creates custom colors.
  * @Michael Yang
  * @11/12/2021
  */
@@ -13,38 +12,40 @@ import java.util.List; // resolves problem with java.awt.List and java.util.List
 public class SheparFaireyLab
 {
     /**
-     * main method, to test the picture
+     * Picture face0 = new Picture("images/shepard2.jpg"); //opens the original picture
+     * face0.explore();
      *  
      */
     public static void main(String[] args)
     {
         
-         //opens selfie picture 
-          /**/
-         Picture face1 = new Picture("images/shepard1.jpg");         
-         Picture face2 = new Picture("images/shepard.jpg");
-         Picture face3 = new Picture("images/shepard1.jpg");        
+          
+         Picture face1 = new Picture("images/shepard2.jpg");  //creates the picture for the first method    
+         Picture face2 = new Picture("images/shepard2.jpg");  //creates the picture for the first method    
+         Picture face3 = new Picture("images/shepard2.jpg");  //creates the picture for the first method          
          
-         Pixel pixels1 [] = face1.getPixels();
+         Pixel pixels1 [] = face1.getPixels(); 
          Pixel pixels2 [] = face2.getPixels();
          Pixel pixels3 [] = face3.getPixels();
          
-         Color lightgreen = new Color(199,223,137);
-         Color lightyellow = new Color(242,190,87);
-         Color lightblue = new Color(104,121,159);
-         Color pink = new Color(189,116,159);
+        
          
-         int r,g,b,average;
+         Color lightgreen = new Color(199,223,137); //custom color pallet -> lightgreen color
+         Color lightyellow = new Color(242,190,87); //custom color pallet -> light yellow color
+         Color lightblue = new Color(104,121,159);  //custom color pallet -> light blue color
+         Color pink = new Color(189,116,159);   //custom color pallet -> pink color
          
-         //method 3
+         int r,g,b,average; //creates the variables for red, green, blue, and average (grayscale)
          
-         for (Pixel spot : pixels3) {
+         //method 3 //custom color pallet
+         
+         for (Pixel spot : pixels3) { //creates a for loop that scans the picture and grabs the colors and the average int
              r = spot.getRed();
              g = spot.getGreen();
              b = spot.getBlue();
              average = (int) (r+b+g)/3;
              
-             if (average <= 63) 
+             if (average <= 63) //divides the spectrum of 255 into 4 integer quadrants, and sets a custom color for each quadrant
                 spot.setColor(pink); 
              else if (average <= 127) 
                 spot.setColor(lightblue);
@@ -54,43 +55,90 @@ public class SheparFaireyLab
                  spot.setColor(lightgreen);
          }    
              
-         face3.explore();
+         face3.explore(); //displays the image output
+         
+         
    
+        //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+          
+        
+          //method 2
+         int small = 255; //sets the smallest value as the lightsest color (white)
+         int big = 0; //sets the biggest value as the darkest color (black)
+         
+         Color darkBlue = new Color(10,42,43); //creates a dark blue color
+         Color medBlue = new Color(101,147,160); //creates a medium blue color
+         Color lightBlue = new Color(185,204,184); //creates a light blue color
+         Color yellow = new Color(255,239,167); //creates a yellow color
+         Color red = new Color(219,21,34); //creates a red color
+         
+         for (Pixel spot : pixels2) { //creates a for loop that first grabs the colors then sets the average to the highest and lowest grayscale intensities
              
-         //method 2
-         int min = 255;
-         int max = 0;
-         for (Pixel spot : pixels2) {
-             r = spot.getRed();
-             g = spot.getGreen();
-             b = spot.getBlue();
-             average = (int) (r+b+g)/3;
              
-             spot.setColor(new Color(average, average, average)); 
-             if (min > spot.getRed()) {
-                 min = spot.getRed();
-             if (max < spot.getRed()) {
-                 max = spot.getRed();
+         r = spot.getRed();
+         g = spot.getGreen();
+         b = spot.getBlue();
+         average = (int) (r+b+g)/3;
+
+             
+             if (average > big) {
+                 big = average;
+             }
+             else if (small > average) {
+                 small = average;
+             }
+  
          }    
          
-         face2.explore();
+         for (Pixel spot : pixels2) { //creates another for loop that divides the grayscale intensities into 4 quadrants and sets each quadrant to a special color
+             r = spot.getRed();
+             b = spot.getBlue();
+             g = spot.getGreen();
+             average = (r+b+g)/3;
+             
+             if (average > (big - small)/4*3 + small) {
+                 spot.setColor(medBlue);
+                }
+                
+             else if (average > (big - small)/4*2 + small) {
+                 spot.setColor(lightBlue);
+                }
+             
+             else if (average > (big - small)/4 + small) {
+                 spot.setColor(yellow);
+             }
+             
+             else
+                 spot.setColor(red);
+                 
+            
+            }
+
+             
+             
+         face2.explore(); //displays the iamge output
          
-         //method1
+        
+        //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- 
          
-         Color darkBlue = new Color(10,42,43);
-         Color medBlue = new Color(101,147,160);
-         Color lightBlue = new Color(185,204,184);
-         Color yellow = new Color(255,239,167);
-         Color red = new Color(219,21,34);
+        
+        
+        //method1
+         
+         darkBlue = new Color(10,42,43); //creates the designated color pallet
+         medBlue = new Color(101,147,160);
+         lightBlue = new Color(185,204,184);
+         yellow = new Color(255,239,167);
+         red = new Color(219,21,34);
                    
-         for (Pixel spot : pixels1) {
+         for (Pixel spot : pixels1) { //uses a for loop to scan the image and grab the rgb color
              r = spot.getRed();
              g = spot.getGreen();
              b = spot.getBlue();
 
-             average = (int) (r+b+g)/3;
+             average = (int) (r+b+g)/3; //sets the average (grayscale)
              
-             if (average <= 51) 
+             if (average <= 51) //divides the spectrum into 5 quadrants, and sets a designated color for each quadrant
                 spot.setColor(darkBlue); 
              else if (average <= 102) 
                 spot.setColor(medBlue);
@@ -103,11 +151,17 @@ public class SheparFaireyLab
              
          }    
              
-         face1.explore();
+         face1.explore(); //displays the image output
+         
+         
+        //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+         //saves the images to the images folder under their respective names
           
-         face1.write("images/method1.jpg");
+         face1.write("images/method1.jpg"); 
+         face2.write("images/method2.jpg");
          face3.write("images/method3.jpg");
          
     }//main       
 }//class
+
 
