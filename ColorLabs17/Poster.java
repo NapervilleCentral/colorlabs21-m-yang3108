8a2947b3518f6de2218cd86097e443a8950a40cb
Michael Yang <mryang@stu.naperville203.org>
2022-05-18 14:23:20 -0500

    dasdewafa

--- ColorLabs17/Poster.java
+++ ColorLabs17/Poster.java
@@ -9,16 +9,18 @@ import java.util.List;
  */
 public class Poster
 {
-    // instance variables - replace the example below with your own
+    // instance variables
     private int x;
     private int r, g, b;
 
     /**
      * Constructor for objects of class Poster
+     * Sets up a 3*2 collage of spongebobs
+     * also calls the 6 methods
      */
     public static void main (String [] args)
     {
-        Picture apic = new Picture("images\\streets.jpg");
+        Picture apic = new Picture("images\\spongebob.jpg");
         //creates a 3x2 collage of the poster by creating a new picture
         Picture poster = new Picture(apic.getWidth() * 3, apic.getHeight() * 2);
         for (int y = 0; y < poster.getHeight(); y++)
@@ -26,13 +28,17 @@ public class Poster
             {
                 poster.getPixel(x, y).setColor(apic.getPixel(x % apic.getWidth(), y % apic.getHeight()).getColor());   
             }
-        negated(poster);    
-        grayscale(poster);
-        mirror(poster,  apic.getWidth(), 0, apic.getWidth(), apic.getHeight());
+        //negated(poster, 0, 0, apic.getWidth(), apic.getHeight());    
+        //grayscale(poster, apic.getWidth() * 2, apic.getHeight() * 2, apic.getWidth(), apic.getHeight());
+        mirror(poster,  apic.getHeight(), apic.getWidth(), apic.getHeight(), apic.getWidth());
+        posterize(poster, 0, 0, apic.getHeight(), apic.getWidth());
         poster.explore();
     }
-
-    public static void grayscale(Picture s)
+    /**
+     * Grayscale method
+     * uses 
+     */
+    public static void grayscale(Picture s, int startX, int startY, int w, int h )
     {
         int height = s.getHeight();
         int width = s.getWidth();
@@ -40,9 +46,9 @@ public class Poster
         Pixel target = null;
         Pixel source = null;
 
-        for (int r = 0; r < width; r++)
+        for (int r = startY; r < startY+h; r++)
         {
-            for (int c = 0; c < height; c++)
+            for (int c = startX; c < startX+w; c++)
             {
                 source = s.getPixel(r,c);
                 int r1 = source.getRed();
@@ -67,12 +73,11 @@ public class Poster
         Pixel target = null;
         Pixel source = null;
 
-        for (int r = 0; r < width; r++)
+        for (int r = startY; r < startY + h; r++)
         {
-            for (int c = 0; c < height; c++)
+            for (int c = startX; c < startX+w; c++)
             {
-                
-                
+
                 source = s.getPixel(r,c);
                 int r1 = source.getRed();
                 int g1 = source.getGreen();
@@ -83,10 +88,10 @@ public class Poster
 
         }
     }
-    
+
     public static void mirror(Picture p, int startX, int startY, int w, int h)
     {
-        
+
         for (int y = startY; y < startY+h; y++)
         {
             for (int x = startX; x < startX+w; x++)
@@ -101,9 +106,49 @@ public class Poster
             }
         }
 
-        
-        
-        
     }
-    
+
+    public static void upsidedown(Picture p, int startX, int startY, int w, int h)
+    {
+
+    }
+
+    public static void recursion(Picture p, int startX, int startY, int w, int h)
+    {
+
+    }
+
+    public static void posterize (Picture p, int startX, int startY, int w, int h)
+    {
+        Pixel source = null;
+        for (int y = startY; y < startY+h; y++)
+        {
+            for (int x = startX; x < startX+w; x++)
+
+            {
+                Color darkBlue = new Color(10,42,43); //creates the designated color pallet
+                Color medBlue = new Color(101,147,160);
+                Color lightBlue = new Color(185,204,184);
+                Color yellow = new Color(255,239,167);
+                Color red = new Color(219,21,34);
+                source = p.getPixel(y,x);
+                int r2 = source.getRed();
+                int g2 = source.getGreen();
+                int b2 = source.getBlue();
+
+                int average = (int) (r2+b2+g2)/3; //sets the average (grayscale)
+
+                if (average <= 51) //divides the spectrum into 5 quadrants, and sets a designated color for each quadrant
+                    source.setColor(darkBlue); 
+                else if (average <= 102) 
+                    source.setColor(medBlue);
+                else if (average <= 153) 
+                    source.setColor(lightBlue);
+                else if (average <= 204)
+                    source.setColor(yellow);
+                else if (average <= 255) 
+                    source.setColor(red);
+            }
+        }
+    }
 }
